e4f7eda6200366f6c7264e6a0c940c2dc5d47725
Dawid Byczkowski <dbyczkowski@gmail.com>
2018-10-08 13:16:39 +0200

    Fix some bugs

--- Reverse words/src/Kata.java
+++ Reverse words/src/Kata.java
@@ -5,21 +5,25 @@
 //    Examples
 //    "This is an example!" ==> "sihT si na !elpmaxe"
 //    "double  spaces"      ==> "elbuod  secaps"
-
 class Kata {
 
     public static void main(String[] args) {
         System.out.println(reverseWords("This is an example!"));
+        System.out.println(reverseWords("double  spaces"));
     }
-    
+
     public static String reverseWords(final String original) {
         String[] tableOfString = original.split(" ");
         StringBuilder builder = new StringBuilder();
+        if (tableOfString.length > 0) {
         for (int i = 0; i < tableOfString.length; i++) {
-            builder.append(new StringBuilder(tableOfString[i]).reverse().toString());
-            builder.append(" ");
+                builder.append(new StringBuilder(tableOfString[i]).reverse().toString());
+                builder.append(" ");
+            }
+        } else {
+            return original;
         }
-        builder.deleteCharAt(original.length());
+        builder.deleteCharAt(builder.length() - 1);
         return builder.toString();
     }
 }
