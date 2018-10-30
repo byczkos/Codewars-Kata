2cac242301bc3522831d97ba1757d39b694ad729
Dawid Byczkowski <dbyczkowski@gmail.com>
2018-10-30 13:28:36 +0100

    Bumps in the road

---
+++ Bumps in the Road/src/BumpsTheRoad.java
@@ -0,0 +1,33 @@
+
+//    Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about
+//    15 more bumps before it dies totally.
+//
+//    Unfortunately for you, your drive is very bumpy! Given a string showing either flat road ("_") or
+//    bumps ("n"), work out if you make it home safely. 15 bumps or under, return "Woohoo!", over 15 bumps
+//    return "Car Dead".
+public class BumpsTheRoad {
+
+//    === check the solution ===
+    public static void main(String[] args) {
+        System.out.println(bumps("n"));
+        System.out.println(bumps("nnnnnnnnnnnnnnnnnnnnn"));
+        System.out.println(bumps("______n___n_"));
+        System.out.println(bumps("_nnnnnnn_n__n______nn__nn_nnn"));
+        System.out.println(bumps("nn__nn_n___n___nnn_"));
+        System.out.println(bumps("_n___nn__n_n___"));
+    }
+
+//    in my opinion solution works good, but random test is failed 
+    public static String bumps(final String road) {
+        int counter = 0;
+        for (int i = 0; i < road.length(); i++) {
+            if (road.charAt(i) == 'n' || road.charAt(i) == '_') {
+                counter++;
+            }
+        }
+        if (counter > 15) {
+            return "Car Dead";
+        }
+        return "Woohoo!";
+    }
+}
