a7d8ba0fc4d9c5b98cab992acaae046423a37cfa
Dawid Byczkowski <dbyczkowski@gmail.com>
2019-01-24 15:47:30 +0100

    Double Cola

---
+++ Java/Double Cola/src/Line.java
@@ -0,0 +1,48 @@
+//        Sheldon, Leonard, Penny, Rajesh and Howard are in the queue for a "Double Cola" drink vending machine; there are no other people in the queue.
+//                The first one in the queue (Sheldon) buys a can, drinks it and doubles! The resulting two Sheldons go to the end of the queue.
+//                Then the next in the queue (Leonard) buys a can, drinks it and gets to the end of the queue as two Leonards, and so on.
+//
+//        For example, Penny drinks the third can of cola and the queue will look like this:
+//
+//        Rajesh, Howard, Sheldon, Sheldon, Leonard, Leonard, Penny, Penny
+//        Write a program that will return the name of the person who will drink the n-th cola.
+//
+//        Input
+//        The input data consist of an array which contains at least 1 name, and single integer n.
+//
+//        1  ≤  n  ≤  1000000000
+//        Output / Examples
+//        Return the single line — the name of the person who drinks the n-th can of cola. The cans are numbered starting from 1.
+//
+//        string[] names = new string[] { "Sheldon", "Leonard", "Penny", "Rajesh", "Howard" };
+//        Line.WhoIsNext(names, 1) == "Sheldon"
+//        Line.WhoIsNext(names, 52) == "Penny"
+//        Line.WhoIsNext(names, 7230702951) == "Leonard"
+
+//        URL: https://www.codewars.com/kata/double-cola/train/java
+
+public class Line {
+
+    public static void main(String[] args) {
+        System.out.println(Line.WhoIsNext(new String[]{"Sheldon", "Leonard", "Penny", "Rakesh", "Howard"}, 1));
+        System.out.println(Line.WhoIsNext(new String[]{"Sheldon", "Leonard", "Penny", "Rakesh", "Howard"}, 52));
+//        System.out.println(Line.WhoIsNext(new String[]{"Sheldon", "Leonard", "Penny", "Rakesh", "Howard"}, 7230702951));
+    }
+
+    public static String WhoIsNext(String[] names, int n) {
+        int j = 0;
+        if (n <= 5 && n >= 1) {
+            return names[n - 1];
+        } else {
+            for (int i = 6; i < n; i++) {
+                if (i % 2 == 0) {
+                    j++;
+                }
+                if (j == 5) {
+                    j = 0;
+                }
+            }
+            return names[j - 1];
+        }
+    }
+}
